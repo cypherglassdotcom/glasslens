ef8c822caab5c429d871092d7e3521570a7ab402
leordev <leordev@gmail.com>
2018-06-14 05:46:50 -0400

    basic flow

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # glassvote
 Cypherglass EOS Voting Tool
+
+## Voting Flow
+
+1. Open app
+2. Immediately display the list of BPs, with Cypherglass autoselected on top
+3. They click on BPs and then click vote
+4. App prompts to disconnect network
+5. App shows a online/offline status, if the user stays online in this step it
+warns the user
+6. They enter private key — `this screen could be integrated with the
+future ledger/trezor hd wallets`
+7. Transaction is created
+8. Then they go back online and submit the transaction
+9. Popups message saying no private info was sent over the network or
+written to hard drive, and pk has been destroyed
+10. Option to close the application or start a new voting session
--- src/Main.elm
+++ src/Main.elm
@@ -1,48 +1,11 @@
 module Main exposing (..)
 
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Html exposing (Html, text, div, h1, img, p, a, nav, span)
+import Html.Attributes exposing (src, class, attribute)
+import Html.Events exposing (onClick)
 
 
----- MODEL ----
-
-
-type alias Model =
-    {}
-
-
-init : ( Model, Cmd Msg )
-init =
-    ( {}, Cmd.none )
-
-
-
----- UPDATE ----
-
-
-type Msg
-    = NoOp
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    ( model, Cmd.none )
-
-
-
----- VIEW ----
-
-
-view : Model -> Html Msg
-view model =
-    div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
-        ]
-
-
-
----- PROGRAM ----
+---- PROGRAM & INITS ----
 
 
 main : Program Never Model Msg
@@ -53,3 +16,178 @@ main =
         , update = update
         , subscriptions = always Sub.none
         }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( initialModel, Cmd.none )
+
+
+
+---- MODEL ----
+
+
+type Step
+    = Welcome
+    | ListBps
+    | EnterPk
+    | TransactionConfirmation
+    | SuccessFinal
+
+
+type alias Producer =
+    { account : String
+    , rank : Int
+    , votes : Float
+    , percentage : Float
+    , url : String
+    }
+
+
+type alias Model =
+    { producers : List Producer
+    , step : Step
+    , pk : Maybe String
+    }
+
+
+initialModel : Model
+initialModel =
+    { producers = []
+    , step = Welcome
+    , pk = Nothing
+    }
+
+
+apiUrl : String
+apiUrl =
+    "http://api.cypherglass.com:8888"
+
+
+
+---- UPDATE ----
+
+
+type Msg
+    = StartVoting
+    | ConfirmVote
+    | GenerateTransaction
+    | ConfirmTransaction
+    | NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        StartVoting ->
+            ( { model | step = ListBps, pk = Nothing }, Cmd.none )
+
+        ConfirmVote ->
+            ( { model | step = EnterPk }, Cmd.none )
+
+        GenerateTransaction ->
+            ( { model | step = TransactionConfirmation }, Cmd.none )
+
+        ConfirmTransaction ->
+            ( { model | step = SuccessFinal }, Cmd.none )
+
+        NoOp ->
+            ( model, Cmd.none )
+
+
+
+---- VIEW ----
+
+
+view : Model -> Html Msg
+view model =
+    case model.step of
+        Welcome ->
+            welcomeView model
+
+        ListBps ->
+            listBpsView model
+
+        EnterPk ->
+            enterPkView model
+
+        TransactionConfirmation ->
+            transactionView model
+
+        SuccessFinal ->
+            successView model
+
+
+welcomeView : Model -> Html Msg
+welcomeView model =
+    div [ class "welcome" ]
+        [ img [ src "/logo-cg.svg" ] []
+        , h1 [] [ text "Welcome to Cypherglass Voting Tool!" ]
+        , p [] [ text "This is a safe and easy tool to vote for EOS Block Producers" ]
+        , a [ onClick StartVoting ] [ text "Start Voting Session" ]
+        ]
+
+
+pageView : Model -> List (Html msg) -> Html msg
+pageView model content =
+    div []
+        [ topMenu model
+        , div [ class "container" ] content
+        ]
+
+
+topMenu : Model -> Html msg
+topMenu model =
+    nav
+        [ attribute "aria-label" "main navigation"
+        , class "navbar topcg"
+        , attribute "role" "navigation"
+        ]
+        [ div [ class "navbar-brand logo" ]
+            [ img [ class "logo-img", src "/logo_horizontal.svg" ] []
+            , span [ class "title-span is-hidden-mobile" ] [ text "VOTING TOOL" ]
+            , span [ class "title-span is-hidden-tablet" ] [ text "VT" ]
+            ]
+        , div [ class "navbar-menu" ]
+            [ div [ class "navbar-end" ]
+                [ text "" ]
+            ]
+        ]
+
+
+listBpsView : Model -> Html Msg
+listBpsView model =
+    pageView model
+        [ h1 [] [ text "Producers List" ]
+        , p [] [ text "Please vote Wisely!" ]
+        , a [ onClick ConfirmVote ] [ text "Confirm Vote" ]
+        ]
+
+
+enterPkView : Model -> Html Msg
+enterPkView model =
+    pageView model
+        [ h1 [] [ text "Enter your Private Key" ]
+        , p [] [ text "It's safer if you disconnect" ]
+        , a [ onClick GenerateTransaction ] [ text "Confirm Voting Transaction" ]
+        ]
+
+
+transactionView : Model -> Html Msg
+transactionView model =
+    pageView model
+        [ h1 [] [ text "Review Vote" ]
+        , p [] [ text "Confirm your Vote Transaction data:" ]
+        , p [] [ text "A, B, C" ]
+        , a [ onClick ConfirmTransaction ] [ text "Submit Vote" ]
+        ]
+
+
+successView : Model -> Html Msg
+successView model =
+    pageView model
+        [ h1 [] [ text "Success" ]
+        , p [] [ text "Thanks for voting using Cypherglass Voting Tool!" ]
+        , p [] [ text "Share voting in Twitter | Facebook" ]
+        , a [ onClick StartVoting ] [ text "New Voting Session" ]
+        ]
